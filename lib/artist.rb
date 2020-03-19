34d4ae7c5a18cf682319e8f39bf95e606904fd91
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 20:04:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,67 @@
 require 'pry'
+
 class Artist
-  attr_accessor :name, :songs
+
+  attr_accessor :name, :songs, :add_song, :print_songs, :find_or_create_by_name
 
   @@all = []
 
   def initialize(name)
     @name = name
-    save
+    @@all << self
+    self
   end
 
-    def add_song(song)
-      # binding.pry
-      song.artist = self
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    Song.new(song.name)
+    song.artist = self
+  end
+
+  def songs
+    @songs = []
+
+       Song.all.each do |song|
+            if song.artist != nil
+                if song.artist.name == self.name
+                 @songs << song
+                end
+            end
+       end
+
+    @songs = @songs.uniq
+
+  end
+
+  def self.find_or_create_by_name(find_artist)
+    binding.pry
+    artists = @@all
+    n = 0
+    artists.each do |artist|
+      puts artist
+      if artist.name == find_artist
+        n+=1
+        return artist
+      end
+      unless n >= 1
+        artist = Artist.new(find_artist)
+        return artist
+      end
+
     end
 
-    def songs
-      Song.all.select {|song| song.artist == self }
-    end
+  end
 
-    def save
-      @@all << self
+  def print_songs
+    songs = self.songs.uniq
+    songs.each do |song|
+      puts song.name
+binding.pry
     end
+  end
 
-    def self.all
-      @@all
-    end
-
-    def self.find_or_create_by_name(artist_name)
-        artist = self.all.find {|artist| artist.name == artist_name}
-        if artist
-          artist
-        else
-          new_artist = self.new(artist_name)
-        end
-    end
-
-    def print_songs
-      songs.each { |song| puts song.name}
-    end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Song
 
   attr_accessor :name, :artist
@@ -13,20 +11,6 @@ class Song
 
   def self.all
     @@all
-  end
-
-  def self.new_by_filename(filename)
-    # binding.pry
-    song = filename.split(" - ")[1]
-    artist = filename.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name= artist
-    new_song
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
   end
 
 end
