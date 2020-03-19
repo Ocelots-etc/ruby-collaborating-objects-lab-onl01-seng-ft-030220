e34a5b3c3963466a5ad2b7f529eef572f8211b53
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 21:04:21 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,11 +9,11 @@ class MP3Importer
   def files
     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
     # set an instance variable that is equal to all the possible paths that music can come in on
-    
+
   end
 
   def import
     files.each {|song| Song.new_by_filename(song)}
-    # call the files method and loop through each file and create a 
+    # call the files method and loop through each file and create a
   end
 end
