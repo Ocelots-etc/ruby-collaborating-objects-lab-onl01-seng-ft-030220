f47b1ddd37d45efb926e716ddea0d610ea746bc4
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 20:53:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,6 +25,7 @@ class Song
     # artist - file_pieces[0]
     # song = file_pieces[1]
     # .strip takes all special characters and extra junk data
+    # like in scrapping instead of API
   end
 
 end
