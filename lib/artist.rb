a91e791d24c7f9cd019b83567cac7a17aefc30f8
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 22:27:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,7 @@ class Artist
     self.all.find { |artist| artist.name == name }
   end
 
-  def print_songs(artist)
+  def print_songs
     # songs = self.songs.uniq
     artist.songs.each { |song| puts song.artist.name }
   end
