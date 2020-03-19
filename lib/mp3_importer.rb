15ee227ccbde27af1f32aa1805e2f9aebf89b0f6
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 21:09:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,10 @@ class MP3Importer
   def files
     # @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
     # set an instance variable that is equal to all the possible paths that music can come in on
-    @files ||= Dir.glob("#{path}/*.mp3")
+    @files ||= Dir.glob("#{path}/*.mp3").collect do |file|
+
+
+    end
     # /*.mp3 escapes the space
 
   end
